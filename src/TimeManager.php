9ad1e9ca80ee1e96e4905304242927a3689518f3
pixelwhiz <daffa01futn@gmail.com>
2024-10-21 22:46:33 +0700

    initial commit

--- src/TimeManager.php
+++ src/TimeManager.php
@@ -37,35 +37,51 @@ class TimeManager {
         return "Sunday";
     }
 
+    public static function rangeOfDay(string $day): array {
+        $index = array_search($day, DayRange::DAYS);
+        if ($index === false) {
+            throw new \InvalidArgumentException("Invalid day: $day");
+        }
+
+        switch ($index) {
+            case 0:
+                return DayRange::SUNDAY;
+            case 1:
+                return DayRange::MONDAY;
+            case 2:
+                return DayRange::TUESDAY;
+            case 3:
+                return DayRange::WEDNESDAY;
+            case 4:
+                return DayRange::THURSDAY;
+            case 5:
+                return DayRange::FRIDAY;
+            case 6:
+                return DayRange::SATURDAY;
+            default:
+                throw new \OutOfBoundsException("Invalid index: $index");
+        }
+    }
+
     public static function getCurrentTime(World $world): string {
         if (self::getCurrentDay($world) === DayRange::DAYS[0]) {
             $worldTime = floatval($world->getTime());
             if ($worldTime >= DayRange::SUNDAY[2] and $worldTime < DayRange::SUNDAY[3]) {
                 $time = ($worldTime - DayRange::SUNDAY[2]) * 0.06;
-                $time = round($time); // Membulatkan waktu ke integer
+                $time = round($time);
                 return Time::format(gmdate("i:s", $time), $time);
             } else if ($worldTime >= DayRange::SUNDAY[0] and $worldTime < DayRange::SUNDAY[1]) {
                 $time = ($worldTime * 0.06) + 360;
-                $time = round($time); // Membulatkan waktu ke integer
-                return Time::format(gmdate("i:s", $time), $time);
-            }
-        } else {
-            $worldTime = intval($world->getTime());
-            $currentDay = self::getCurrentDay($world);
-            $days = [
-                DayRange::MONDAY,
-                DayRange::TUESDAY,
-                DayRange::WEDNESDAY,
-                DayRange::THURSDAY,
-                DayRange::FRIDAY,
-                DayRange::SATURDAY,
-            ];
-
-            foreach ($days as $day) {
-                $time = ($day[1] - $worldTime) * 0.05;
+                $time = round($time);
                 return Time::format(gmdate("i:s", $time), $time);
             }
         }
+
+        $worldTime = floatval($world->getTime());
+        $currentDay = self::getCurrentDay($world);
+        $time = ($worldTime - self::rangeOfDay($currentDay)[0]) * 0.06;
+        $time = round($time);
+        return Time::format(gmdate("i:s", $time), $time);
     }
 
 }
--- src/handlers/EventHandler.php
+++ src/handlers/EventHandler.php
@@ -3,6 +3,7 @@
 namespace pixelwhiz\times\handlers;
 
 use pixelwhiz\times\TimeManager;
+use pocketmine\event\entity\EntityTeleportEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerItemHeldEvent;
 use pocketmine\event\player\PlayerItemUseEvent;
@@ -17,6 +18,15 @@ class EventHandler implements Listener {
             $day = TimeManager::getCurrentDay($player->getWorld());
             $time = TimeManager::getCurrentTime($player->getWorld());
             $player->sendMessage("Time: {$day}, {$time}");
+        }
+    }
+
+    public function onChange(EntityTeleportEvent $event) {
+        $from = $event->getFrom()->getWorld()->getFolderName();
+        $to = $event->getTo()->getWorld()->getFolderName();
+        $toWorld = $event->getTo()->getWorld();
+        if ($from !== $to) {
+
         }
     }
 
